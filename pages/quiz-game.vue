c92c854df718d8b2523a0caba04d97c5ef986dd9
danielcao <mycaoyue@gmail.com>
2018-09-02 13:07:45 +0100

    style(project): improve styles

--- pages/quiz-game.vue
+++ pages/quiz-game.vue
@@ -1,3 +1,31 @@
 <template lang="pug">
-  vue-particles(color="#fff")
+  v-layout(
+    class="center-contents"
+    justify-center 
+    align-center
+  )  
+    v-btn(
+      class="test"
+      color="purple"
+      fab
+      large
+      dark
+    )
+      v-icon(
+        large
+      ) play_arrow
+    vue-particles(
+      class="test"
+      color="#fff"
+      :hoverEffect="false"
+      :clickEffect="false"
+    )
 </template>
+
+<style lang="sass" scoped>
+.center-contents > *
+  position: absolute
+
+.center-contents > button
+  z-index: 2;
+</style>
--- plugins/components.js
+++ plugins/components.js
@@ -1,6 +1,4 @@
 import Vue from 'vue';
 import NuxtLogo from '~/components/NuxtLogo';
-import VuetifyLogo from '~/components/VuetifyLogo';
 
 Vue.component(NuxtLogo.name, NuxtLogo);
-Vue.component(VuetifyLogo.name, VuetifyLogo);
